e3a05557d6bff7963c5a79a34b52166fb16a66a6
nat <nat.foster@gmail.com>
2017-11-16 15:58:18 -0700

    easier trigger file

--- setup-replication.sh
+++ setup-replication.sh
@@ -14,7 +14,7 @@ else
 cat > ${PGDATA}/recovery.conf <<EOF
 standby_mode = on
 primary_conninfo = 'host=${REPLICATE_FROM} port=5432 user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}'
-trigger_file = '/tmp/touch_me_to_promote_to_me_master'
+trigger_file = '/tmp/make_me_master'
 EOF
 chown postgres ${PGDATA}/recovery.conf
 chmod 600 ${PGDATA}/recovery.conf
